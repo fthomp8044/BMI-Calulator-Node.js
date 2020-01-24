f00623626c00d79ba027b3dd40251b6caf1cbe77
fthomp8044 <fthomp2430@gmail.com>
2020-01-23 22:56:41 -0500

    added bmi weight and height

--- calculator.js
+++ calculator.js
@@ -1,16 +1,31 @@
-
+//jshint esverson:6
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmicalculator");
+})
+
+app.post("/bmicalculator", function(req, res){
+  var weight = req.body.weight;
+  var height = req.body.height;
+
+  var result = 
+})
+
 app.post("/", function(req, res){
-  res.send("<h1>Thx for coming</h1>");
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+
+  var results = num1 + num2;
+    res.send("The result is " + results);
 })
 app.listen(3000, function(){
   console.log("your server starts a port 3000");
